ea661239aab1fea359ead84b543518d485bca2e0
kuangnan <770534880@qq.com>
2019-01-17 17:21:04 +0800

    添加小程序接口applet

---
+++ src/main/java/com/hust/bmzsweb/managesystem/controller/applet/ActivityController.java
@@ -0,0 +1,14 @@
+package com.hust.bmzsweb.managesystem.controller.applet;
+
+/**
+ *  查看活动详情（加入浏览历史）
+ *  搜索活动 默认按热度排行
+ *  发起活动（系统内部审核报名信息 过滤字符串）
+ *  发起者修改活动信息
+ *  删除活动
+ *  收藏活动
+ */
+public class ActivityController {
+
+
+}
