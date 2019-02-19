83f9f46ba6759bc1fe46ee0b80dd670e88c3e5ad
kuangnan <770534880@qq.com>
2019-02-19 17:26:31 +0800

    增加部分shiro代码，还待完善

--- src/main/java/com/hust/bmzsweb/managesystem/ManagesystemApplication.java
+++ src/main/java/com/hust/bmzsweb/managesystem/ManagesystemApplication.java
@@ -2,6 +2,7 @@ package com.hust.bmzsweb.managesystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class ManagesystemApplication {
